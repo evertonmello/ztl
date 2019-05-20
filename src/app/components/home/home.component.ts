a999ccd24916ee475fef1d0efa2259b511728eec
Everton Luiz Leite De Mello <evertonmello@brq.com>
2019-05-20 13:19:50 -0300

    imagem favoritos com gradient parametrizavel

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-home',
@@ -7,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-
-  constructor() { }
+  url = "/../assets/img/bey.jpg"
+  cards = [1,2,3,4,5,6]
+  friendsFavs = [1,2,3,4,5,6]
+  constructor(private _sanitizer: DomSanitizer) {}
 
   ngOnInit() {
   }
+
+  getBackground(image) {
+      return this._sanitizer.bypassSecurityTrustStyle(`linear-gradient(180deg, rgba(0, 0, 0, 0) 0%,
+      rgba(0, 0, 0, 0.150) 50%,    
+      rgba(0, 0, 0, 0.350) 70%,    
+      rgba(0, 0, 0, 0.771) 95%,    
+      rgba(0, 0, 0, 0.801) 100%), url(${image})`);
+  }
+
 
 }
