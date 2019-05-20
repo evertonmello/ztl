79b000e47263ac1c634992769f963fd9a98e84d5
everton.mello <everton.mello@airstrip.com.br>
2019-05-20 12:45:07 -0300

    ajuste img baackground

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,8 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
- 
-  url = '../../../assets/img/bey.jpg';
 
   constructor() { }
 
--- src/app/components/widgets/gradient-img/gradient-img.component.ts
+++ src/app/components/widgets/gradient-img/gradient-img.component.ts
@@ -14,18 +14,7 @@ export class GradientImgComponent implements OnInit {
     console.log(this.url)
   }
 
-  setMyStyle(){
-    let style = {
-      
-      'background-color':'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.150) 50%,rgba(0, 0, 0, 0.350) 70%,rgba(0, 0, 0, 0.771) 95%, rgba(0, 0, 0, 0.801) 100%), url(' + this.url +') no-repeat',
-      'background-size': '98px',
-      'height': '95px',
-      'width': '95px',
-      'border-radius': '5px',
-      'margin-right': '8px'
-    }
-    return style;
-  }
+
 }
 
 
