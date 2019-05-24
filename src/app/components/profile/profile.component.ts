3c9d35820fee732fd3bac0820d494214c26070d4
Everton Luiz Leite De Mello <evertonmello@brq.com>
2019-05-24 15:43:59 -0300

    tela profile v1

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router'
 
 @Component({
@@ -8,17 +9,24 @@ import { ActivatedRoute } from '@angular/router'
 })
 export class ProfileComponent implements OnInit {
 
+  friendsFavs = [1,2,3,4,5,6,7,8];
+  url = "./assets/img/bey.jpg"
   profileId;
-  constructor(private activatedRoute:ActivatedRoute) {
+  constructor(private _sanitizer: DomSanitizer,private activatedRoute:ActivatedRoute) {
     this.activatedRoute.queryParams.subscribe((params) =>{
-      console.log(params)
-
       this.profileId = params.profileId
     })
    }
 
   ngOnInit() {
-    console.log(this.profileId)
   }
+
+  getBackground(image) {
+    return this._sanitizer.bypassSecurityTrustStyle(`linear-gradient(180deg, rgba(0, 0, 0, 0) 0%,
+    rgba(0, 0, 0, 0.150) 50%,    
+    rgba(0, 0, 0, 0.350) 70%,    
+    rgba(0, 0, 0, 0.771) 95%,    
+    rgba(0, 0, 0, 0.801) 100%), url(${image}) `);
+}
 
 }
