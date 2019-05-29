ee6104d4dd60d4efc27827573f393a9471c3e0dd
Everton Luiz Leite De Mello <evertonmello@brq.com>
2019-05-29 19:16:59 -0300

    import image quilljs

--- src/app/components/new-post/new-post.component.ts
+++ src/app/components/new-post/new-post.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, ɵConsole } from '@angular/core';
 import { ActivatedRoute, Router} from '@angular/router';
 
-import { Camera, CameraOptions } from '@ionic-native/camera'
 
 declare var Quill;
 var Delta = Quill.import('delta');
@@ -36,8 +35,8 @@ export class NewPostComponent implements OnInit {
     var toolbarOptions = [
       ['bold'],['italic'], ['underline'],[ 'strike'],  
       [ { 'list': 'bullet' }],
-      ['image'],
-      [{ 'align': [] }]
+      [{ 'align': [] }],
+      ['image']
     ];
     this.postDraft = new Quill('#post', {
       modules: { toolbar: toolbarOptions },
@@ -72,53 +71,7 @@ export class NewPostComponent implements OnInit {
     this.pubPost = !this.pubPost;
   }
 
-  openGallery(){
-    var srcType = Camera.PictureSourceType.SAVEDPHOTOALBUM;
-    var options = this.setOptions(srcType);
-    var func = this.createNewFileEntry;
-    var func2 = this.createFile
-    var draft = this.postDraft;
-    navigator.camera.getPicture(function cameraSuccess(imageUri) {
-
-      func(imageUri, function (dirEntry) {
-        func2(dirEntry, "temp.jpg", false, (fileEntry)=>{
-          console.log(draft)
-          console.log(fileEntry.nativeURL)
-          //draft.insertEmbed(1, 'image', fileEntry.nativeURL);
-        })
-      })
-    }, function cameraError(error) {
-      console.debug("Unable to obtain picture: " + error, "app");
-
-    }, options);
-  }
-
-  createNewFileEntry(imgUri, callback) {
-    var resolvFunc = window['resolveLocalFileSystemURL'];
-    resolvFunc(cordova.file.cacheDirectory, function success(dirEntry) {
-      callback(dirEntry);
-    }, null);
-  }
-  createFile(dirEntry, fileName, isAppend, callback) {
-    dirEntry.getFile(fileName, { create: true, exclusive: false }, function (fileEntry) {
-      callback(fileEntry)
-    }, null);
-  }
 
 
-  setOptions(srcType) {
-    var options = {
-      // Some common settings are 20, 50, and 100
-      quality: 100,
-      destinationType: Camera.DestinationType.FILE_URI,
-      // In this app, dynamically set the picture source, Camera or photo gallery
-      sourceType: srcType,
-      encodingType: Camera.EncodingType.JPEG,
-      mediaType: Camera.MediaType.PICTURE,
-      allowEdit: true,
-      correctOrientation: true  //Corrects Android orientation quirks
-    }
-    return options;
-  }
 
 }
